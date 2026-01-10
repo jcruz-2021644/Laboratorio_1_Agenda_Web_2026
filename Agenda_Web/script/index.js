5d8a9ab3976b580002bf488377668881907b4adb
jcruz-2021644 <jcruz-2021644@kinal.edu.gt>
2026-01-10 17:50:39 -0600

    Se agrego la funcion para que aparesca la imagen en la ventana emergente

--- Agenda_Web/script/index.js
+++ Agenda_Web/script/index.js
@@ -1,20 +1,17 @@
 const modal = document.getElementById("contactModal");
 const closeBtn = document.querySelector(".close");
 
-function openModal(name, phone, address) {
+window.openModal = function (name, phone, address, photo) {
+
     document.getElementById("modalName").textContent = name;
     document.getElementById("modalPhone").textContent = phone;
     document.getElementById("modalAddress").textContent = address;
 
+    const img = document.getElementById("modalPhoto");
+    img.src = photo;
+    img.alt = "Foto de " + name;
+
     modal.style.display = "block";
-}
+};
 
-closeBtn.onclick = function () {
-    modal.style.display = "none";
-}
-
-window.onclick = function (event) {
-    if (event.target === modal) {
-        modal.style.display = "none";
-    }
-}
+closeBtn.onclick = () => modal.style.display = "none";
