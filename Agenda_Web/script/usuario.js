11485557ed6f6c8fd64d10653224b5f4381d34a3
jcruz-2021644 <jcruz-2021644@kinal.edu.gt>
2026-01-12 07:51:11 -0600

    Se agrego el js para que el login sea funcional y tambien el js para que se setene los datos en la vista usuario

---
+++ Agenda_Web/script/usuario.js
@@ -0,0 +1,12 @@
+// Obtener usuario guardado
+    const userData = JSON.parse(localStorage.getItem('userData'));
+
+    if (userData) {
+        document.getElementById('username').value = userData.user;
+        document.getElementById('email').value = userData.email;
+        document.getElementById('password').value = userData.password;
+    } else {
+        // Si no hay usuario logueado
+        alert("Debes iniciar sesión");
+        window.location.href = "login.html"; // ajusta ruta
+    }
